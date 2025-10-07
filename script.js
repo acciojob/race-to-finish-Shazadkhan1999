ef8afb5178280e5840fce847a9c90fe33711ddc8
Shazadkhan1999 <Shazadkhan40005@gmail.com>
2025-10-07 19:24:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 window.promises = [];
 
-const promises = [];
+//const promises = [];
 
-for(int i = 1; i <= 5; i++){
-	const randomTime = Math.floor(Math.random() = *5+1)*1000;
-	const promise = new Promise ((resolve) => ) {
+for(let i = 1; i <= 5; i++){
+	const randomTime = Math.floor(Math.random()  *5+1)*1000;
+	const promise = new Promise ((resolve) => {
 		setTimeout(() => {
-		resolve(`Promise ${i} resolved after ${(randomTime() / 1000} seconds`);
+		resolve(`Promise ${i} resolved after ${randomTime / 1000} seconds`);
 		}, randomTime);
 	});
-	promises.push(promise);
+	window.promises.push(promise);
 	}
 
-Promise.any(promise)
+Promise.any(window.promises)
 .then((result) => {
 	document.getElementById("output").textContent = result;
 })
